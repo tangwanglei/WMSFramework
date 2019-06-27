5c289ae8bc37e6b763b666f181eb9e7871bbf8fb
twl <tangwanglei@outlook.com>
2019-06-27 14:26:19 +0800

    添加项目文件。

---
+++ WMS.Infrastucture/DefaultTags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WMS.Infrastucture
+{
+    /// <summary>
+    /// 标签类
+    /// </summary>
+    public class DefaultTags
+    {
+
+    }
+}
---
+++ WMS.Infrastucture/RegionType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WMS.Infrastucture
+{
+    /// <summary>
+    /// 窗体遮罩层和TabPage层
+    /// </summary>
+    public class RegionType
+    {
+        /// <summary>
+        /// 窗体遮罩层
+        /// </summary>
+        public const string SurfaceRegion = "SurfaceRegion";
+        /// <summary>
+        /// 窗体遮罩层
+        /// </summary>
+        public const string ModifyRegion = "ModifyRegion";
+        /// <summary>
+        /// 窗体主体层
+        /// </summary>
+        public const string BodyRegion = "BodyRegion";
+        /// <summary>
+        /// 窗体主界面TabPage层
+        /// </summary>
+        public const string TabPageRegion = "TabPageRegion";
+        /// <summary>
+        /// 公共主体层
+        /// </summary>
+        public const string CommRegion = "CommRegion";
+    }
+}
